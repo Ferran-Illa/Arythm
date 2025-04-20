d14dabbc30be098711313c4b88e2066787a67b63
aandreaqr <andrea.quesada@estudiantat.upc.edu>
2025-04-20 19:37:01 +0200

    modified:   .vscode/settings.json 	modified:   ODE.c 	modified:   SingleCell.c

--- ODE.c
+++ ODE.c
@@ -3,27 +3,79 @@
 
 #include "include/common.h"
 #include "include/functions.h"
+#include "include/ODE.h" 
+ // --------------------------- ODE DEFINITION -------------------------
+ // param=[tv+, tv1-, tv2-, tw-, td, t0, tr, tsi, k, Vsic, Vc, Vv]
+ // y=[V, v, w]
+ // p= H(V-param[10])   ; q= H(V-param[11])
 
+double H(double x) {
+    if (x < 0) {
+        return 0.0; // Return 0 for negative inputs
+    } else {
+        return 1.0; // Return 1 for non-negative inputs
+    }
+}
+double tauvminus(double *y, double *param) {
+        return (1 - H(y[0] - param[11])) * param[1] + H(y[0] - param[11]) * param[2];
+    }
+
+double Ifi(double *y, double *param) {
+        return ( -y[1]*H(y[0]-param[10])*(y[0]-param[10])*(1-y[0])/param[4]);
+    }
+
+double Iso(double *y, double *param) {
+        return ( y[0]*(1-H(y[0]-param[10]))/param[5]+H(y[0]-param[10])/param[6]);
+    }
+
+double Isi(double *y, double *param) {
+        return ( -y[2]*(1+tanh(param[8]*(y[0]-param[9])))/(2*param[7]));
+    }
+
+
+void ODE_func(double t, double *y, double *dydt, double *param) { // Represents a function for solving ordinary differential equations (ODEs)
+    dydt[0] = - Ifi(y, param) - Iso(y, param) - Isi(y, param);
+    dydt[1] = (1 - H(y[0] - param[10])) * (1 - y[1]) / tauvminus(y, param) - H(y[0] - param[10]) * y[1] / param[0];
+}
+
+
+
+
+
+ 
 // ---------------------------- ODE SOLVER ---------------------------
 
-Matrix euler_integration(ODEFunction ode_func, double step_size, int num_steps, double initial_t, double initial_y) {
+typedef void (*ODEFunction)(double, double *, double *, double *); // Ensure ODEFunction matches ODE_func signature
+
+
+Matrix euler_integration_multidimensional(ODEFunction ODE_func, double step_size, int num_steps, double initial_t, double *initial_y, int dim, double *param) {
     double t = initial_t;
-    double y = initial_y;
-    Matrix result = create_matrix(2, num_steps); // 2 rows, for t and y
+    double y[dim]; // Current state
+    double dydt[dim]; // Derivatives
+    for (int i = 0; i < dim; i++) {
+        y[i] = initial_y[i]; // Initialize y
+    }
+
+    Matrix result = create_matrix(dim + 1, num_steps); // rows: 1 for t, dim for y
 
     for (int i = 0; i < num_steps; i++) {
-        MAT(result, 0, i) = t;
-        MAT(result, 1, i) = y;
-        //printf("Step %d: t = %f, y = %f\n", i, t, y); // If we need to print all steps
-        y += step_size * ode_func(t, y);
-        t += step_size;
+        MAT(result, 0, i) = t; // Store time
+        for (int j = 0; j < dim; j++) {
+            MAT(result, j + 1, i) = y[j]; // Store y values
+        }
+
+        // Compute derivatives
+        ode_func(t, y, dydt, param); // Call the ODE function to compute derivatives
+
+        // Update y using Euler's method
+        for (int j = 0; j < dim; j++) {
+            y[j] += step_size * dydt[j];
+        }
+
+        t += step_size; // Update time
     }
     return result;
 }
 
-double expODE(double t, double y) {
-    return -y; // dy/dt = -y
-}
-
-
-#endif // ODE_H
+// End of ODE_H guard
+#endif
--- SingleCell.c
+++ SingleCell.c
@@ -1,5 +1,6 @@
 #include "include/common.h"
 #include "include/functions.h"
+#include "include/ODE.h" // Include the header file for ODE_func
 
 // ----------------------------- MAIN -----------------------------
 
@@ -9,8 +10,9 @@ void main()
     double step_size = 0.1;
     int num_steps = 10;
     double initial_t = 0.0;
-    double initial_y = 10.0;
-    Matrix M = euler_integration(expODE, step_size, num_steps, initial_t, initial_y);
+    double initial_y[] = {10.0, 10.0, 10.0};
+    double param[] = {3.33, 10.0, 3.33, 3.33, 3.33, 3.33, 10.0, 13.03, 3.33, 10}; // Example parameters
+    Matrix M = euler_integration(ODE_H, step_size, num_steps, initial_t, initial_y[0]);
 
     print_matrix(&M); // Print the matrix for debugging
 
@@ -18,13 +20,15 @@ void main()
         Cast the first row (time) to t and the second row (ode values) to y, 
         vectors are read linearly which makes casting rows to vectors feasible.
         Temporary solution untila proper vectorization is implemented.  
-    */
+    
     Vector t; Vector y;
     t.size = num_steps;
     y.size = num_steps;
     t.data = M.data;
     y.data = M.data + num_steps; // Offset to the second row
     // Plot the results
-    plot_with_sdl(&t, &y);
-    free_matrix(&M); // Free the matrix after use, vectors are freed too with this action.
-}
+    // plot_with_sdl(&t, &y);
+    // free_matrix(&M); // Free the matrix after use, vectors are freed too with this action.
+    */
+    Matrix result= euler_integration_multidimensional(ODE_H, step_size, num_steps, initial_t, initial_y, 3, param);
+    }
