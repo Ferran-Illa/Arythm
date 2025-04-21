1be244b51e24dd9070855284d99fc0883c118a8d
Ferran Illa <ferran.illa@illavera.com>
2025-04-21 20:27:58 +0200

    Grid has been added to the UI
     Changes to be committed:
    	modified:   SingleCell.sh
    	modified:   UI.c

--- UI.c
+++ UI.c
@@ -47,40 +47,44 @@ void plot_with_sdl(const Vector *x, const Vector *y, const double *axes) {
 
     // Determine scaling factors and offsets to fit data within the window
     double x_min = axes[0], x_max = axes[1], y_min = axes[2], y_max = axes[3];
+
     double x_scale = window_width / (x_max - x_min);
     double y_scale = window_height / (y_max - y_min);
 
-    // Draw grid and ticks
-    SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255); // Light gray for the grid
-    double xtick = (x_max - x_min) / 10; // Example: 10 ticks on x-axis
-    double ytick = (y_max - y_min) / 10; // Example: 10 ticks on y-axis
-
-    for (double x_tick = x_min; x_tick <= x_max; x_tick += xtick) {
-        int x_pos = (int)((x_tick - x_min) * x_scale);
-        SDL_RenderDrawLine(renderer, x_pos, 0, x_pos, window_height); // Vertical grid line
-    }
-
-    for (double y_tick = y_min; y_tick <= y_max; y_tick += ytick) {
-        int y_pos = window_height - (int)((y_tick - y_min) * y_scale);
-        SDL_RenderDrawLine(renderer, 0, y_pos, window_width, y_pos); // Horizontal grid line
-    }
-
     // Draw axes
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Black for the axes
-    
-    int x_axis_pos = window_height - (int)((0.0 - y_min) * y_scale); // Y=0 line
-    int y_axis_pos = (int)((0.0 - x_min) * x_scale); // X=0 line
+
+    int x_axis_pos = abs(y_min * y_scale); // Y=0 line
+    int y_axis_pos = abs(x_min * x_scale); // X=0 line
 
     SDL_RenderDrawLine(renderer, 0, x_axis_pos, window_width, x_axis_pos); // X-axis
     SDL_RenderDrawLine(renderer, y_axis_pos, 0, y_axis_pos, window_height); // Y-axis
+
+    // Draw grid and ticks
+    SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255); // Light gray for the grid
+    int htick = (int)( x_scale * (x_max - x_min) / 10 ); // Example: about 10 ticks
+    int vtick = (int)( y_scale * (y_max - y_min) / 10 ); // Example: about 10 ticks
+
+    for (int v_pos = y_axis_pos + vtick; v_pos <= window_width; v_pos += vtick) {
+        SDL_RenderDrawLine(renderer, v_pos, 0, v_pos, window_height); // Right Vertical grid line
+    }
+    for (int h_pos = x_axis_pos + htick; h_pos <= window_height; h_pos += htick) {
+        SDL_RenderDrawLine(renderer, 0, h_pos, window_width, h_pos); // Top Horizontal grid line
+    }
+    for (int v_pos = y_axis_pos - vtick; v_pos >= 0; v_pos -= vtick) {
+        SDL_RenderDrawLine(renderer, v_pos, 0, v_pos, window_height); // Left Vertical grid line
+    }
+    for (int h_pos = x_axis_pos - htick; h_pos >= 0; h_pos -= htick) {
+        SDL_RenderDrawLine(renderer, 0, h_pos, window_width, h_pos); // Bottom Horizontal grid line
+    }
 
     // Draw the plot
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // Blue for the plot
     for (int i = 0; i < x->size - 1; i++) {
         int x1 = (int)((x->data[i] - x_min) * x_scale);
-        int y1 = window_height - (int)((y->data[i] - y_min) * y_scale);
+        int y1 = (int)((y->data[i] - y_min) * y_scale);
         int x2 = (int)((x->data[i + 1] - x_min) * x_scale);
-        int y2 = window_height - (int)((y->data[i + 1] - y_min) * y_scale);
+        int y2 = (int)((y->data[i + 1] - y_min) * y_scale);
         if (SDL_RenderDrawLine(renderer, x1, y1, x2, y2) != 0) {
             fprintf(stderr, "SDL_RenderDrawLine Error: %s\n", SDL_GetError());
         }
@@ -99,6 +103,7 @@ void plot_with_sdl(const Vector *x, const Vector *y, const double *axes) {
             }
         }
     }
+    // We could support fullscreen
 
     // Clean up
     SDL_DestroyRenderer(renderer);
